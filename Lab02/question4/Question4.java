1721036af42c87aaf5f4c5bbf71573d3380fc724
tarunraghav226 <tarun.raghav226@gmail.com>
2019-08-19 15:20:57 +0530

    Add constraints and output statement and task done

--- Lab02/question4/Question4.java
+++ Lab02/question4/Question4.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
-public class Question3{
+public class Question4{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int val1= scan.nextInt();
+        if(val1==0){
+            System.out.println("Zero");
+        }
+        else if(val1>0){
+            System.out.println("Positive");        
+        }
+        else{
+            System.out.println("Negative");
+        }
         scan.close();
     }
 }
