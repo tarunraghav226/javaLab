18f61d73b48b475d2f56624be8dd33b541155ca4
tarunraghav226 <tarun.raghav226@gmail.com>
2019-08-19 14:52:27 +0530

    Add output statement  and question completed

--- Lab02/question2/question2.java
+++ Lab02/question2/question2.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
-public class Question2{
-    public static void main()Strin[] args) {
+public class question2{
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String val1= scan.next();
+        String val2 = scan.next();
+        System.out.println(val1+" University "+val2);
         scan.close();
     }
 }
