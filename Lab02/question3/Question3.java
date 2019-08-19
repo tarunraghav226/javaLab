94bc6441d0e93dda27707a6605401b96e839053b
tarunraghav226 <tarun.raghav226@gmail.com>
2019-08-19 15:13:16 +0530

    Add outuput statement and question completed

--- Lab02/question3/Question3.java
+++ Lab02/question3/Question3.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 
-public class question2{
+public class Question3{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int val1= scan.nextInt();
+        int val2 = scan.nextInt();
+        System.out.println(val1+" + "+val2+" = "+(val1+val2));
+        System.out.println(val1+" - "+val2+" = "+(val1-val2));
+        System.out.println(val1+" * "+val2+" = "+(val1*val2));
+        System.out.println(val1+" / "+val2+" = "+(val1/val2));
+        System.out.println(val1+" % "+val2+" = "+(val1%val2));
         scan.close();
     }
 }
