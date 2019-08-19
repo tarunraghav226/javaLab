9ef5c40ad347be18a8fd70a4eaad95ec5b198490
tarunraghav226 <tarun.raghav226@gmail.com>
2019-08-19 15:25:09 +0530

    Add constraints and output statement and task done

--- Lab02/question5/Question5.java
+++ Lab02/question5/Question5.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 
-public class Question4{
+public class Question5{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int val1= scan.nextInt();
+        if(val1%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");        
+        }
         scan.close();
     }
 }
