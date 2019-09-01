c5a0da5b0c9119496fcaeab409cf4674f5c375d6
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-02 00:47:46 +0530

    add : solution for problem and final commit

--- Lab03/question3/Question3.java
+++ Lab03/question3/Question3.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class Question3{
-    
+    double tempInFarenheit;
+    Question3(double temp){
+        tempInFarenheit=temp;
+    }
+    double calculateCelsisus(){
+        return (tempInFarenheit-32)/1.8;
     }
     public static void main(String []args){
         Scanner scan =new Scanner(System.in);
-
+        Question3 obj=new Question3(scan.nextDouble());
+        System.out.println(obj.calculateCelsisus());
         scan.close();
     }
 }
