54fb0f04da6ee920138b387e1b22225b8e47dce6
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-02 14:52:20 +0530

    add: initial commit Main.java execution file

---
+++ Lab04/question1/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(Strin[] args){
+        Person obj =new Person("tarun",18,'M');
+        Scanner scan = new Scanner(System.in);
+        
+        scan.close();
+    }
+}
