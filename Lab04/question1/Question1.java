ec8c3e61a1447ebd53dfbf17c9a63a1447c93b38
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-02 14:38:20 +0530

    add: definition of Person class

--- Lab04/question1/Question1.java
+++ Lab04/question1/Question1.java
@@ -1,4 +1,40 @@
 import java.util.Scanner;
 
 public class Person{
+
+    private String name;
+    private int age;
+    private char gender;
+
+    //methods of Person class 
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public char getGender(){
+        return gender;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public void setGender(char gender){
+        this.gender=gender;
+    }
+
+    public Person(String name,int age,char gender){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
 }
