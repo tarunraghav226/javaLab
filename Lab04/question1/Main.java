1e9a2f54c9ba23b6ff40a74c164653baa8da29c4
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-02 14:59:00 +0530

    add : 1>compiled file of person class 2>add toString object in Main.java

--- Lab04/question1/Main.java
+++ Lab04/question1/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(Strin[] args){
+    public static void main(String[] args){
         Person obj =new Person("tarun",18,'M');
         Scanner scan = new Scanner(System.in);
-        
+        System.out.print(obj.toString());
         scan.close();
     }
 }
