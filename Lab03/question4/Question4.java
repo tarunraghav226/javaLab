546e3438b8c50da0064cffab132fd061f98710bf
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-02 14:20:13 +0530

    add: problem solution and final commit

--- Lab03/question4/Question4.java
+++ Lab03/question4/Question4.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
 
 public class Question4{
+    int n;
+    Question4(int n){
+        this.n=n;
+    }
+    void pattern(){
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
-        Scanner scan = new Scanner();
+        Scanner scan = new Scanner(System.in);
+        Question4 obj =new Question4(scan.nextInt());
+        obj.pattern();
         scan.close();
     }
 }
