d2ee0c8d19fa71d1f0c7a249f1c3a25bb1a000d6
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-16 15:42:00 +0530

    init commit

--- Lab05/bottleNeckQuestion/Main.java
+++ Lab05/bottleNeckQuestion/Main.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main{
-    public static void main(String [] args){
-        int n;
-        Scanner scan= new Scanner(System.in);
-        n=scan.nextInt();
-        int []a= new int[n];
-        for(int i=0;i<n;i++){
-            a[i]=scan.nextInt();
-        }
-        for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
-        }
-    }
+   int n;
+   int []a;
+   Main(){
+       Scanner scan =new Scanner(System.in);
+       n=scan.nextInt();
+       a=new int[n];
+       for(int i=0;i<n;i++){
+           a[i]=scan.nextInt();
+       }
+       scan.close();
+       System.out.println("Hello World");
+   }
+   public void print(){
+       for(int i=0;i<n;i++)
+        System.out.println(a[i]);
+   }
+   public static void main(String []args){
+       Main obj=new Main();
+   }
 }
