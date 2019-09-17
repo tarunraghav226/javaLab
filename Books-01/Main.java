550712f1bfd7828312b2564bed741b1763457113
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-17 19:11:54 +0530

    add: definition class

--- Books-01/Main.java
+++ Books-01/Main.java
@@ -2,8 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    
+    //class variable
+    static int noOfBooks;
+    
+    //instance variable
+    private String bookName;
+    private String authorName;
+    private String ISBN;
+    
+    //Constructor
+    Solution(String bName,String aName,String ISBN){
+        bookName=bName;
+        authorName=aName;
+        this.ISBN=ISBN;
+    }
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    //getters
+    public String getAuthorName(){
+        return authorName;
+    }
+
+    public String getBookName(){
+        return bookName;
+    }
+
+    public String getISBN(){
+        return ISBN;
+    }
+
+    public String toString(){
+        return "-----------------------------"+"\n"+
+               "Book Name:    "+getBookName()+"\n"+
+               "Author Name:    "+getAuthorName()+"\n"+
+               "ISBN:    "+getISBN()+"\n"+ 
+               "-----------------------------"+"\n";
     }
 }
