4f60ac75c5ea1e87594761c685561f03f06cf83f
tarunraghav226 <tarun.raghav226@gmail.com>
2019-09-23 15:58:29 +0530

    added email and password generator

--- Lab06/Mail.java
+++ Lab06/Mail.java
@@ -1,16 +1,38 @@
-
+import java.util.Random;
 
 public class Mail{
     private String name;
     private String company;
     private String age;
+    private String password;
+    private String email;
     Mail(String n,String a,String c){
         name = n;
         company = c;
         age = a;
+        password=createPass();
     }
 
-    public String toString(){
-        return String.format("%10s_%2s@%s.com",name.toLowerCase(),age,company);
+    private String createPass(){
+        email=String.format("%s_%s@%s.com",name.toLowerCase(),age,company);
+        Random rand = new Random();
+        String s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String sp="~`!@#$%^&*(){}|[]\\:;\"'<,>.?/+";
+        String pass="";
+        pass = pass +s.charAt(Math.abs(rand.nextInt(50))%26);
+        pass+=Math.abs(rand.nextInt(10));
+        pass+=Math.abs(rand.nextInt(10));
+        pass+=Math.abs(rand.nextInt(10));
+        pass+=Math.abs(rand.nextInt(10));
+        if(name.length()>4)
+            pass+=name.substring(0,5);
+        else    
+            pass+="00";
+        pass+=sp.charAt(Math.abs(rand.nextInt())%20);
+        return pass;
+    }
+
+    public String  getEmailAndPass(){
+        return "email -- "+email+"\n"+"pass -- "+password;
     }
 }
--- Lab06/Main.java
+++ Lab06/Main.java
@@ -5,7 +5,7 @@ class Main{
         int n = Integer.parseInt(scan.nextLine());
         for(int i=0;i<n;i++){   
             Mail obj =new Mail(scan.next(),scan.next(),scan.next());
-            System.out.println(obj.toString());
+            System.out.println(obj.getEmailAndPass());
             }
         scan.close();
     }
