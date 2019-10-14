3b79ba4d798cdbdbeb716ac20bcb2517453c724d
tarunraghav226 <tarun.raghav226@gmail.com>
2019-10-14 15:30:51 +0530

    added arraylist of float

--- Lab09/StringDemo.java
+++ Lab09/StringDemo.java
@@ -1,15 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class StringDemo{
     public static void main(String[] args){
-        String[] names= new String[5];
-        for (int i = 0; i < names.length; i++) {
-            names[i]=new String("Tarun version--> 1."+i);
-        }
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
-        Arrays.fill(names,"Tarun");
-        System.out.println(Arrays.toString(names));
+        ArrayList<Float> array = new ArrayList<Float>();
+        array.add(23f);
+        System.out.println(array.toString());
     }
 }
