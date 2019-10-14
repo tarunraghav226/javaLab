dc70a02caa24312a4a8216b042f24df43d5361cc
tarunraghav226 <tarun.raghav226@gmail.com>
2019-10-14 15:06:44 +0530

    added string array

--- Lab09/StringDemo.java
+++ Lab09/StringDemo.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class StringDemo{
     public static void main(String[] args){
-        boolean [] bool=new boolean[5];
-        for(boolean b : bool){
-            System.out.println(b);
+        String[] names= new String[5];
+        for (int i = 0; i < names.length; i++) {
+            names[i]=new String("Tarun version--> 1."+i);
         }
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        Arrays.fill(names,"Tarun");
+        System.out.println(Arrays.toString(names));
     }
 }
