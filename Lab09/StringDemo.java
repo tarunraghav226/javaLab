9fb5b85748b14507aa150380ad5a22d79124dbd1
tarunraghav226 <tarun.raghav226@gmail.com>
2019-10-14 14:41:28 +0530

    added boolean array

---
+++ Lab09/StringDemo.java
@@ -0,0 +1,8 @@
+public class StringDemo{String[] args){
+    public static void main(String[] args){
+        boolean [] bool=new boolean[5];
+        for(boolean b : bool){
+            System.out.println(b);
+        }
+    }
+}
